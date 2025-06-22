171d677760d99f6f2e5912791b16078a10b4f29e
Vladimir N. <lismiroslav@yandex.ru>
2025-06-22 15:38:04 +0300

    Fix formatting in style.css by removing unnecessary line and ensuring consistent styling

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 import { QrcodeStream } from 'vue-qrcode-reader';
+import './style.css'
 
 const app = createApp(App);
 app.use(createPinia());
